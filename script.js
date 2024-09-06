3034eaead0ac93b6fcf206277b2fbe6fe26a5888
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2024-09-06 21:53:41 +0200

    done start screen, removed log, finished base game

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let food = { x: 15, y: 15 };
 let direction = "right";
 let score = 0;
 let highScore = 0;
+let gameStarted = false;
+let intervalID = 0;
 
 // Draw snake
 function draw() {
@@ -36,7 +38,6 @@ function drawSnake() {
 
 // Draw food
 function drawFood() {
-  //TODO: generate random positon in the grid
   let xPosition = food.x;
   let yPosition = food.y;
 
@@ -74,6 +75,11 @@ function move() {
 }
 
 function gameControls(event) {
+  if (gameStarted === false && event.code === "Space") {
+    start();
+    gameStarted = true;
+  }
+
   switch (event.key) {
     case "w":
       direction = "up";
@@ -91,13 +97,8 @@ function gameControls(event) {
 }
 
 function collisionDetection() {
-  //snake head position
-  console.log(snake[0].x);
-  console.log(snake[0].y);
-
   //collision with board
   if (snake[0].x >= GRID_SIZE || snake[0].x <= 0) {
-    console.log("collision!!!! x-way");
     if (score > highScore) {
       highScore = score;
       highScoreElement.textContent = highScore.toString().padStart(3, "0");
@@ -106,7 +107,6 @@ function collisionDetection() {
   }
 
   if (snake[0].y >= GRID_SIZE || snake[0].y <= 0) {
-    console.log("collision!!!! y-way");
     if (score > highScore) {
       highScore = score;
       highScoreElement.textContent = highScore.toString().padStart(3, "0");
@@ -115,29 +115,16 @@ function collisionDetection() {
   }
 
   if (snake[0].x === food.x && snake[0].y === food.y) {
-    console.log("snake on food");
     //generate new (x,y) position for food -> random x,y position that is in the GRID_SIZE
     food.x = Math.floor(Math.random() * GRID_SIZE + 1);
     food.y = Math.floor(Math.random() * GRID_SIZE + 1);
 
     //add element to the snake
-    snake.push({ x: 0, y: 0 });
+    snake.push({ x: snake[0].x, y: snake[0].y });
 
     score += 1;
     scoreElement.textContent = score.toString().padStart(3, "0");
   }
-}
-
-// Testing drawing and moving the snake
-document.addEventListener("keydown", gameControls);
-drawFood();
-
-function gameLoop() {
-  const interval = setInterval(() => {
-    move();
-    collisionDetection();
-    draw();
-  }, 250);
 }
 
 function reset() {
@@ -145,8 +132,26 @@ function reset() {
   score = 0;
   scoreElement.textContent = score.toString().padStart(3, "0");
   snake = [startPosition]; //snake is an array with the object that saves the (x,y) position of each snake cube.
-  food = { x: 15, y: 15 };
-  clearInterval(interval);
+  food = { x: 10, y: 10 };
+
+  clearInterval(intervalID);
+  gameStarted = false;
+
+  document.querySelector("#logo").style.display = "block";
+  document.querySelector("#instruction-text").textContent =
+    "Press spacebar to start the game";
 }
 
-gameLoop();
+// Testing drawing and moving the snake
+document.addEventListener("keydown", gameControls);
+
+function start() {
+  document.querySelector("#logo").style.display = "none";
+  document.querySelector("#instruction-text").textContent = "";
+
+  intervalID = setInterval(() => {
+    move();
+    collisionDetection();
+    draw();
+  }, 250);
+}
